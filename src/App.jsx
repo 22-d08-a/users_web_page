bd6be4fa74a157665cab0886099cf84ccb7fc7c2
Wilhelmine <74241434+wilhelmine-erber@users.noreply.github.com>
2023-05-11 13:01:02 +0200

    Initialize localstorage

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import "./App.css";
+import "./styles/app.scss";
+
+function App() {
+  const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      console.log(data);
+      localStorage.setItem("users", JSON.stringify(data));
+    };
+    fetchData();
+  }, []);
+
+  return <div className="App"></div>;
+}
+
+export default App;
